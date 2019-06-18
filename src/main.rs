b535cc124531cb30e3d52e83dd47dd806feb1815
Ivo Georgiev <ivo@strem.io>
2019-06-18 18:06:17 +0300

    the thing

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,36 @@
-#![feature(async_await, await_macro, futures_api)]
-
 
 use {
     hyper::{
-        Body, Client, Request, Response, Server, Uri,
-
+        Body, Request, Response, Server, Error,
         service::service_fn,
-
         rt::run,
     },
-    futures::{
-        future::{FutureExt, TryFutureExt},
-    },
+    futures::{Future, future},
     std::net::SocketAddr,
-
-    tokio::await,
 };
 
-const MANIFEST: &[u8] = include_bytes!("./manifest.json");
+const RESP_BODY: &[u8] = include_bytes!("./resp.json");
 
-async fn serve_req(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    Ok(Response::new(Body::from(MANIFEST)))
+fn on_request(_: Request<Body>) -> impl Future<Item=Response<Body>, Error=Error> {
+    future::ok(Response::new(Body::from(RESP_BODY)))
 }
 
-async fn run_server(addr: SocketAddr) {
-    println!("Listening on http://{}", addr);
-
-    // Create a server bound on the provided address
-    let serve_future = Server::bind(&addr)
-        // Serve requests using our `async serve_req` function.
-        // `serve` takes a closure which returns a type implementing the
-        // `Service` trait. `service_fn` returns a value implementing the
-        // `Service` trait, and accepts a closure which goes from request
-        // to a future of the response. In order to use our `serve_req`
-        // function with Hyper, we have to box it and put it in a compatability
-        // wrapper to go from a futures 0.3 future (the kind returned by
-        // `async fn`) to a futures 0.1 future (the kind used by Hyper).
-        .serve(|| service_fn(|req|
-            serve_req(req).boxed().compat()
-        ));
-
-    // Wait for the server to complete serving or exit with an error.
-    // If an error occurred, print it to stderr.
-    if let Err(e) = await!(serve_future) {
-        eprintln!("server error: {}", e);
-    }
+fn run_server(addr: &SocketAddr) -> impl Future<Item=(), Error=Error> {
+    Server::bind(addr).serve(|| service_fn(on_request))
 }
 
 fn main() {
-    // Set the address to run our socket on.
-    let addr = SocketAddr::from(([127, 0, 0, 1], 7778));
-
-    let futures_03_future = run_server(addr);
-    let futures_01_future =
-        futures_03_future.unit_error().boxed().compat();
-
-    // Finally, we can run the future to completion using the `run` function
-    // provided by Hyper.
-    run(futures_01_future);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    println!("Listening on http://{}", &addr);
+    run(run_server(&addr)
+        .then(|x| {
+            // we can't .else and panic, cause rustc wants us to return a future
+            // even though it'd be unreachable
+            if let Err(e) = x {
+                panic!(e);
+            }
+            future::ok(())
+        })
+    );
 }
 
