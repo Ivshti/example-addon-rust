c1cf8d08d670662d2bc8b246a64d33190ba20e2d
Ivo Georgiev <ivo@strem.io>
2019-06-18 19:30:03 +0300

    single threaded

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,28 @@
-
-use {
-    hyper::{
-        Body, Request, Response, Server, Error,
-        service::service_fn,
-        rt::run,
-    },
-    futures::{Future, future},
-    std::net::SocketAddr,
-};
+use hyper::{Body, Response, Server, Error};
+use hyper::service::service_fn_ok;
+use hyper::rt::{Future};
+use tokio::runtime::current_thread;
+use std::net::SocketAddr;
 
 const RESP_BODY: &[u8] = include_bytes!("./resp.json");
 
-fn on_request(_: Request<Body>) -> impl Future<Item=Response<Body>, Error=Error> {
-    future::ok(Response::new(Body::from(RESP_BODY)))
-}
-
-fn run_server(addr: &SocketAddr) -> impl Future<Item=(), Error=Error> {
-    Server::bind(addr).serve(|| service_fn(on_request))
-}
-
 fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    println!("Listening on http://{}", &addr);
-    run(run_server(&addr)
-        .then(|x| {
-            // we can't .else and panic, cause rustc wants us to return a future
-            // even though it'd be unreachable
-            if let Err(e) = x {
-                panic!(e);
-            }
-            future::ok(())
-        })
-    );
+
+    let exec = current_thread::TaskExecutor::current();
+
+    let new_service = || service_fn_ok(|_req| Response::new(Body::from(RESP_BODY)));
+    let server = Server::bind(&addr)
+        .executor(exec)
+        .serve(new_service)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    println!("Listening on http://{}", addr);
+
+    current_thread::Runtime::new()
+        .expect("rt new")
+        .spawn(server)
+        .run()
+        .expect("rt run");
 }
 
