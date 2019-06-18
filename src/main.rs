a7c97453ed30a0e9ecbabf90e08f9797a158aa91
Ivo Georgiev <ivo@strem.io>
2019-06-18 22:42:49 +0300

    using raw tokio streams

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,43 @@
-use hyper::{Body, Response, Server, Error};
-use hyper::service::service_fn_ok;
-use hyper::rt::{Future};
-use tokio::runtime::current_thread;
 use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+use tokio::codec;
+use futures::future;
 
-const RESP_BODY: &[u8] = include_bytes!("./resp.json");
+const SINGLE_RESP: &[u8] = b"HTTP/1.1 200 OK\r\nConnection: Keep-Alive\r\nContent-Length: 17\r\n\r\n{\"success\":true}\n";
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    let socket = TcpListener::bind(&addr)?;
+    println!("Listening on: {}", addr);
 
-    let exec = current_thread::TaskExecutor::current();
+    let done = socket
+        .incoming()
+        .map_err(|e| println!("failed to accept socket; error = {:?}", e))
+        .for_each(move |socket| {
+            let (reader, mut writer) = socket.split();
+            let task = codec::FramedRead::new(reader, codec::BytesCodec::new())
+                .for_each(move |bytes| {
+                    // @TODO proper parsing here
+                    let n = bytes.len() / 32;
+                    // @TODO remove unwrap
+                    let resp = (0..n)
+                            .map(|_| SINGLE_RESP)
+                            .flatten()
+                            .cloned()
+                            .collect::<Vec<u8>>();
+                    writer.write(&resp);
+                    //writer.write(&many_resps[0..n*SINGLE_RESP.len()]).unwrap();
+                    future::ok(())
+                });
+            
+            tokio::spawn(task
+                // @TODO: handle errors and etc
+                .then(|_| future::ok(()))
+            )
+        });
 
-    let new_service = || service_fn_ok(|_req| Response::new(Body::from(RESP_BODY)));
-    let server = Server::bind(&addr)
-        .executor(exec)
-        .serve(new_service)
-        .map_err(|e| eprintln!("server error: {}", e));
-
-    println!("Listening on http://{}", addr);
-
-    current_thread::Runtime::new()
-        .expect("rt new")
-        .spawn(server)
-        .run()
-        .expect("rt run");
+    tokio::run(done);
+    Ok(())
 }
 
